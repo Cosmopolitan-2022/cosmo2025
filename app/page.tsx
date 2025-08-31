bbb36aa20dbf66b8e0e43de1ce5e21c02939db3d
narashimha05 <chinnarinarashimha@gmail.com>
2025-09-01 01:15:36 +0530

    Initial

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100 text-black">
-      Hello
+      Hello, let's start
     </div>
   );
 }
