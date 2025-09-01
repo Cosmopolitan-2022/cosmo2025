2b719aaadb863e0245738b2e88cdeaee31c58808
mshalom27 <menshalom07@gmail.com>
2025-09-01 21:15:13 +0530

    responsive till 800px

--- app/Data/NavbarHero/NavbarHero.ts
+++ app/Data/NavbarHero/NavbarHero.ts
@@ -8,7 +8,7 @@ export const navbarHeroData = {
     leftImage: "https://res.cloudinary.com/djqgs2pet/image/upload/v1756717289/OIP_1_bdogwr.png",
     rightImage: "https://res.cloudinary.com/djqgs2pet/image/upload/v1756717296/Group_1000001423_ghfts1.png",
     leftSmoke: "https://res.cloudinary.com/djqgs2pet/image/upload/v1756717296/12_5_qes3f7.png",
-    mainText: "COSMO POLITAN",
-    subText: "Culture is society, heritage, and tradition, the family you are from, the heritage you have, and the traditions you carry forward. The multi-ethnic fest is a celebration of that culture, which will make us one, we will be united in drums and trumpets, singing like the birds in the morning with hope and passion, swayed by the rhythm, and our ancestors will join us in tune. This three-day extravaganza, from 12th Jan to 14nd Jan 2024, will keep you on your toes with many performances and fun events."
+    mainText: "COSMOOOOOO POLITANNNNNN",
+    subText: "Culture is society, heritage, and tradition, the family you are from, the heritage you have, and the traditions you carry forward. The multi-ethnic fest is a celebration of that culture, which will make us one, we will be united in drums and trumpets, singing like the birds in the morning with hope and passion, swayed by the rhythm, and our ancestors will join us in tune. This three-day extravaganza, from 12th Jan to 14nd Jan 2024, will keep you on your toes with many performances and fun events.Culture is society, heritage, and tradition, the family you are from, the heritage you have, and the traditions you carry forward. "
   }
 };
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 import { navbarHeroData } from "../app/Data/NavbarHero/NavbarHero";
 
 function Navbar() {
@@ -7,10 +8,16 @@ function Navbar() {
   const tabs = navbarHeroData.navbar.tabs;
 
   return (
-    <nav className="flex items-center justify-between px-20 py-4 bg-[#1c3c2b]">
-      <div>
-        <img src={logo} alt="Logo" className="h-28 px-10" />
-      </div>
+    <nav className="flex items-center justify-between px-20 py-4  bg-[linear-gradient(to_right,#1c3c2b,#03523C,#1c3c2b)] ">
+      <div className="relative h-12 w-28 sm:h-16 sm:w-36 md:h-20 md:w-44 lg:h-24 lg:w-52 xl:h-28 xl:w-60">
+    <Image
+      src={logo}
+      alt="Logo"
+      fill
+      className="object-contain"
+      priority
+    />
+  </div>
       <ul className="flex space-x-10 text-2xl font-light">
         {tabs.map((tab) => (
           <li key={tab}>
