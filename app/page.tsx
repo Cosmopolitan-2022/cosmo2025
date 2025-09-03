3e19171e3949098e6e94ec25260f8e6b810d8caa
raaz-tilak <raaztilak07@gmail.com>
2025-09-04 02:39:34 +0530

    feat:footer completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Events from "@/components/Events";
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
+import Footer from "@/components/footer/Footer";
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Hero />
       <Events />
+      <Footer />
     </div>
   );
 }
---
+++ components/footer/Address.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { cn } from "@/app/lib/utils";
+import { footerData } from "@/data/footer/footer";
+
+function Address() {
+  const { venue } = footerData;
+  const { sectionsStyle: className } = footerData;
+  return (
+    <div className="w-full ">
+      <h1 className={cn(" text-center", className[0].className)}> VENUE</h1>
+      <div className=" px-4 flex flex-col  ">
+        {venue.map((item) => (
+          <div
+            key={item.id}
+            className={cn(
+              "md:text-base lg:text-[18px] text-center font-semibold capitalize  leading-normal text-[#03523C] "
+            )}
+          >
+            {item.name}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Address;
