f6384e006b0ce9316feea6e5e737dc38c518e2ea
mshalom27 <menshalom07@gmail.com>
2025-09-03 22:51:53 +0530

    border added

--- app/Data/NavbarHero/NavbarHero.ts
+++ app/Data/NavbarHero/NavbarHero.ts
@@ -11,6 +11,8 @@ export const navbarHeroData = {
     centerMobile:"https://res.cloudinary.com/djqgs2pet/image/upload/v1756742522/Pngtree_ethnic_luxury_mandala_golden_art_8786185_1_1_kt4tnc.png",
     mobileHero:"https://res.cloudinary.com/djqgs2pet/image/upload/v1756742713/OIP_1_1_nupqyc.png",
     mainText: "COSMOOOOOO POLITANNNNNN",
-    subText: "Culture is society, heritage, and tradition, the family you are from, the heritage you have, and the traditions you carry forward. The multi-ethnic fest is a celebration of that culture, which will make us one, we will be united in drums and trumpets, singing like the birds in the morning with hope and passion, swayed by the rhythm, and our ancestors will join us in tune. This three-day extravaganza, from 12th Jan to 14nd Jan 2024, will keep you on your toes with many performances and fun events.Culture is society, heritage, and tradition, the family you are from, the heritage you have, and the traditions you carry forward. "
+    subText: "Culture is society, heritage, and tradition, the family you are from, the heritage you have, and the traditions you carry forward. The multi-ethnic fest is a celebration of that culture, which will make us one, we will be united in drums and trumpets, singing like the birds in the morning with hope and passion, swayed by the rhythm, and our ancestors will join us in tune. This three-day extravaganza, from 12th Jan to 14nd Jan 2024, will keep you on your toes with many performances and fun events.Culture is society, heritage, and tradition, the family you are from, the heritage you have, and the traditions you carry forward. ",
+    border1: "https://res.cloudinary.com/djqgs2pet/image/upload/v1756915823/Mask_group_whxibr.png",
+    border2:"https://res.cloudinary.com/djqgs2pet/image/upload/v1756915823/Frame_9496_cxawge.png"
   }
 };
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,17 +11,16 @@ function Hero() {
       leftSmoke,
       mainText,
       subText,
-      centerMobile,
-      mobileHero,
-    },
+      border1,
+      border2},
   } = navbarHeroData;
 
  return (
-  
-    <section className="relative flex flex-col md:flex-row items-center justify-center bg-[linear-gradient(to_right,#1c3c2b,#03523C,#1c3c2b)] w-full ">
+  <>
+    <section className="relative  pt-[50px] pb-[117px] flex flex-col md:flex-row items-center overflow-visible justify-center bg-[linear-gradient(to_right,#1c3c2b,#03523C,#1c3c2b)] w-full">
       <div className="relative w-full md:w-1/2 flex items-center ">
         
-        <div className="absolute  w-[500px] h-[1000px] mt-20 opacity-60">
+        <div className="absolute md:w-[400px] md:h-[800px] lg:w-[500px] lg:h-[1000px] opacity-60">
           <Image
             src={leftBackground}
             alt="Background"
@@ -31,7 +30,7 @@ function Hero() {
           />
         </div>
 
-        <div className="absolute left-0 w-[700px] h-[900px] mt-50">
+        <div className="absolute left-0 md:w-[500px] md:h-[800px] lg:w-[700px] lg:h-[900px] mt-25">
           <Image
             src={leftImage}
             alt="Left image"
@@ -52,8 +51,8 @@ function Hero() {
 
 
       <div className="relative z-30 w-full flex flex-col ml-auto pr-0">
-    <div className="w-full text-justify pr-0">
-      <h1 className="text-5xl md:text-5xl xl:text-9xl font-bold text-[#FFD9A4] text-center">
+    <div className="w-full text-justify pr-8">
+      <h1 className="text-5xl right-8 md:text-5xl xl:text-9xl font-bold text-[#FFD9A4] text-center">
         <span className="block font-[Orbitron]">{mainText.split(" ")[0]}</span>
         <span className="block text-5xl md:text-5xl xl:text-9xl  text-white">
           {mainText.split(" ")[1]}
@@ -61,30 +60,57 @@ function Hero() {
       </h1>
     </div>
 
-    <div className="relative mt-6 w-full flex justify-end">
-      <Image
-        src={rightImage}
-        alt="Hero image"
-        width={180}
-        height={150}
-        className="object-contain relative top-10"
-      />
+<div className="relative mt-6 w-full flex justify-end">
+  <Image
+    src={rightImage}
+    alt="Hero image"
+    width={200} 
+    height={200}
+    className="h-auto"
+  />
 
-      <p className="absolute mt-10 px-48 transform md:text-[16px] lg:text-xl xl:text-2xl text-[#e6e6c7] text-justify pr-12">
-        {subText}
-      </p>
-    </div>
- 
+  <div className="absolute top-0 right-10 flex flex-col items-end p-6 md:p-8 lg:p-x-10">
+    <p className="text-right text-[#e6e6c7] 
+              text-sm md:text-base lg:text-lg xl:text-2xl 
+              max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-4xl">
+  {subText}
+</p>
 
-    <div className="flex md:px-40 lg:px-42 xl:px-48 mt-6 w-full">
-      <button className="px-8 py-3 bg-[#e6e6c7] text-[#1c3c2b] rounded-lg md:text-[20px] lg:text-2xl xl:text-xl shadow font-bold hover:bg-[#d4d4b0] transition">
-        EXPLORE
-      </button>
-    </div>
+    <button className="mt-20 px-8 py-3 bg-[#e6e6c7]/90 text-[#1c3c2b] rounded-lg md:text-[20px] lg:text-2xl xl:text-3xl shadow font-bold">
+      EXPLORE
+    </button>
   </div>
+</div>
+
+  </div>
+
+  
     </section>
 
+ <div className="relative w-screen bg-[linear-gradient(to_right,#1c3c2b,#03523C,#1c3c2b)] overflow-visible">
+  <div className="relative w-screen md:h-8 lg:h-8 xl:h-10 z-20 bg-amber-200">
+    <Image
+      src={border1}
+      alt="Border 1"
+      fill
+      className="object-fill"
+    />
+  </div>
 
+  <div className="relative w-screen md:h-22 lg:h-25 xl:h-30 -mt-10 z-10">
+    <Image
+      src={border2}
+      alt="Border 2"
+      fill
+      className="object-fill"
+    />
+  </div>
+</div>
+
+
+
+
+    </>
   );
 }
 
