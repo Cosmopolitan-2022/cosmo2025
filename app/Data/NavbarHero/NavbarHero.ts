b846f4cf4db80170d70738fd97200f0ea4f5b52c
narashimha05 <chinnarinarashimha@gmail.com>
2025-09-10 18:21:01 +0530

    Add new image assets for the project
    - Added border1.webp and border2.webp for UI borders
    - Included centerMobile.png for mobile layout
    - Added img1.webp for main content
    - Introduced leftBackground.png for background design
    - Added leftImage.png and leftSmoke.png for left section visuals
    - Included logo.png for branding
    - Added mobileHero.png for mobile hero section

--- app/Data/NavbarHero/NavbarHero.ts
+++ app/Data/NavbarHero/NavbarHero.ts
@@ -1,18 +1,18 @@
 export const navbarHeroData = {
-  logo: "https://res.cloudinary.com/djqgs2pet/image/upload/v1756717304/Branding_bjdyuo.png",
+  logo: "/logo.png",
   navbar: {
     tabs: ["Gallery", "About", "Team", "Merch", "Events"]
   },
   hero: {
-    leftBackground: "https://res.cloudinary.com/djqgs2pet/image/upload/v1756717297/Pngtree_ethnic_luxury_mandala_golden_art_8786185_1_dkos5b.png",
-    leftImage: "https://res.cloudinary.com/djqgs2pet/image/upload/v1756717289/OIP_1_bdogwr.png",
+    leftBackground: "/leftBackground.png",
+    leftImage: "/leftImage.png",
     rightImage: "https://res.cloudinary.com/djqgs2pet/image/upload/v1756717296/Group_1000001423_ghfts1.png",
-    leftSmoke: "https://res.cloudinary.com/djqgs2pet/image/upload/v1756717296/12_5_qes3f7.png",
-    centerMobile:"https://res.cloudinary.com/djqgs2pet/image/upload/v1756742522/Pngtree_ethnic_luxury_mandala_golden_art_8786185_1_1_kt4tnc.png",
-    mobileHero:"https://res.cloudinary.com/djqgs2pet/image/upload/v1756742713/OIP_1_1_nupqyc.png",
+    leftSmoke: "/leftSmoke.png",
+    centerMobile:"/centerMobile.png",
+    mobileHero:"mobileHero.png",
     mainText: "COSMO POLITAN",
     subText: "Culture is society, heritage, and tradition, the family you are from, the heritage you have, and the traditions you carry forward. The multi-ethnic fest is a celebration of that culture, which will make us one, we will be united in drums and trumpets, singing like the birds in the morning with hope and passion, swayed by the rhythm, and our ancestors will join us in tune. This three-day extravaganza, from 12th Jan to 14nd Jan 2024, will keep you on your toes with many performances and fun events.Culture is society, heritage, and tradition, the family you are from, the heritage you have, and the traditions you carry forward. ",
-    border1: "https://res.cloudinary.com/djqgs2pet/image/upload/v1756915823/Mask_group_whxibr.png",
-    border2:"https://res.cloudinary.com/djqgs2pet/image/upload/v1756915823/Frame_9496_cxawge.png"
+    border1: "/border1.webp",
+    border2:"/border2.webp"
   }
 };
