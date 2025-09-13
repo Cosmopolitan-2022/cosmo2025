a4c5c9bc1fe60100c2f5daf7ee13741859967d4c
Krishnakant Sahu <krishnakantsahu916@gmail.com>
2025-09-14 00:01:47 +0530

    updated About page route

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,12 @@ import Events from "@/components/Events";
 import Hero from "@/components/Hero";
 import Highlights from "@/components/Highlights";
 import FunEvents from "@/components/Funevents/FunEvents";
-import About from "@/components/About";
 export default function Home() {
   return (
     <div className="min-h-screen">
       <Hero />
       <FunEvents/>
       <Events />
-      <About/>
       <Highlights />
     </div>
   );
