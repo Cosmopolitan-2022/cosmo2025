f648d251a284a8880b29fba1aa95663fb0dd0791
Krishnakant Sahu <krishnakantsahu916@gmail.com>
2025-09-30 00:28:52 +0530

    minor fix

--- components/Sponsor.tsx
+++ components/Sponsor.tsx
@@ -73,7 +73,7 @@ export default function Sponsors() {
       {/* Title */}
       <div className="flex justify-center relative z-10 mb-8 sm:mb-10 md:mb-16 lg:mb-20">
         <h1 className="text-[var(--cream,#FFD9A4)] text-center font-traditional text-5xl xs:text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl font-normal leading-normal tracking-[0.1rem] xs:tracking-[0.15rem] sm:tracking-[0.2rem] md:tracking-[0.3rem] lg:tracking-[0.5rem] xl:tracking-[0.6rem] 2xl:tracking-[0.772rem] px-2 sm:px-4">
-          Sponser
+          Sponsor
         </h1>
       </div>
 
