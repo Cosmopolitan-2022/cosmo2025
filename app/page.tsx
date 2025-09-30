0bbe166bb7b2eb7bccaf2ced98876e03366306af
CosmoNITR <154248513+CosmoNITR@users.noreply.github.com>
2025-09-30 14:55:55 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Events from "@/components/Events";
 import Hero from "@/components/Hero";
 import Highlights from "@/components/Highlights";
 import FunEvents from "@/components/Funevents/FunEvents";
-import Sponsors from "@/components/Sponser";
+import Sponsors from "@/components/Sponsor";
 export default function Home() {
   return (
     <div className="min-h-screen">
